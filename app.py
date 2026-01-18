0220afe1f7bea6c0818e102d63aa409c9597dde0
Mustapha Nafi <nafi.professional@outlook.com>
2026-01-18 17:11:02 +0100

    First version of the flask app

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import socket, datetime
+
+app = Flask(__name__)
+
+@app.route("/main")
+def home():
+    return jsonify({
+        'message': "Welcome to the Home Page!", 
+        'status': "success",
+        'hostname': socket.gethostname(),
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y")
+        })
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+
